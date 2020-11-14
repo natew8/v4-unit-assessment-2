2aca41d449c5164c3cd96570fd8573afa92b8b27
Nathan Waite <natew821@gmail.com>
2020-11-13 19:24:19 -0700

    grrrr

--- js-in-the-dom/index.js
+++ js-in-the-dom/index.js
@@ -1,1 +1,30 @@
 
+function toggler(){
+    const element = document.getElementsByClassName('form')
+    element.classList.toggle('hide')
+}
+
+// const formBtn = document.getElementById('close-form')
+// const form = document.getElementsByClassName('form')
+
+// console.log(form)
+// toggler.addEventListener('click', function(){
+//     if(formBtn.innerText === 'X'){
+//         formBtn.innerText = '+'
+//         form.classList.toggle('.hide')
+//     } else{
+//         formBtn.innerText = 'X'
+//         form.classList.toggle('.form')
+//     }
+// })
+
+// const nameInput = document.querySelector('#name')
+// const emailInput = document.querySelector('#email')
+
+// const emailList = []
+
+// function validateForms(){
+//     if(nameInput.value && emailInput === false){
+//         emailInput
+//     }
+// }
