1496e8c8c3ab67858e768b1d120c95614824b1f2
Nathan Waite <natew821@gmail.com>
2020-11-14 23:05:03 -0700

    DOM Done

--- js-in-the-dom/index.js
+++ js-in-the-dom/index.js
@@ -1,30 +1,73 @@
 
+const formBtn = document.getElementById('close-form')
+const form = document.querySelector('.form')
+
 function toggler(){
-    const element = document.getElementsByClassName('form')
-    element.classList.toggle('hide')
+    if(formBtn.innerText === 'X'){
+        formBtn.innerText = '+'
+    } else{
+        formBtn.innerText = 'X'
+    }form.classList.toggle('hide')
+} 
+formBtn.addEventListener('click', toggler)
+
+
+
+const nameInput = document.querySelector('#name')
+const emailInput = document.querySelector('#email')
+
+const emailList = []
+
+function validateForm(e){
+    if(!nameInput.value && !emailInput.value){
+        emailInput.style.border = '2px solid #ff0000'
+        nameInput.style.border = '2px solid #ff0000'
+        alert('Please enter your name and email address to subscribe!')
+    } else if(!nameInput.value ){
+        nameInput.style.border = '2px solid #ff0000'
+        alert('Please enter your name to subscribe!')
+    }else if(!emailInput.value){
+        emailInput.style.border = '2px solid #ff0000'
+        alert('Please enter your email to subscribe!')
+    }else {
+        emailList.push({'name': nameInput.value, 'email': emailInput.value})
+        displayThankYou()
+    }
 }
+form.addEventListener('submit',(e) => {
+    validateForm()
+    e.preventDefault()
+})
+const formContainer = document.querySelector('.form-container')
 
-// const formBtn = document.getElementById('close-form')
-// const form = document.getElementsByClassName('form')
+function displayThankYou(){
+    formContainer.innerText = 'Thank You!'
+    setTimeout(() => {
+        formContainer.remove()
+    }, 3000);
+}
+nameInput.addEventListener('change', () => {
+    nameInput.style.border = 'none'
+})
+emailInput.addEventListener('change', () => {
+    emailInput.style.border = 'none'
+})
 
-// console.log(form)
-// toggler.addEventListener('click', function(){
-//     if(formBtn.innerText === 'X'){
-//         formBtn.innerText = '+'
-//         form.classList.toggle('.hide')
-//     } else{
-//         formBtn.innerText = 'X'
-//         form.classList.toggle('.form')
-//     }
-// })
 
-// const nameInput = document.querySelector('#name')
-// const emailInput = document.querySelector('#email')
 
-// const emailList = []
+let cart = document.createElement('DIV')
+const main = document.querySelector('.hero')
 
-// function validateForms(){
-//     if(nameInput.value && emailInput === false){
-//         emailInput
-//     }
-// }
+let cartItems = 0
+
+function addToCart(){
+    if(cartItems === 0){
+        cartItems = 1;
+        cart.setAttribute('class','cart-display');
+        cart.innerText = 'Your Cart: 1 item';
+        main.appendChild(cart);
+    } else {
+        cartItems += 1;
+        cart.innerText = `You Cart: ${cartItems} items`
+    }
+};
